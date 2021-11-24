6d4464dcd28bc963a1b6d5d8624f107dad3791b0
Jacob Sapoznikow <jacob1coder@gmail.com>
2021-11-24 21:52:09 +0000

    Add stuff

---
+++ src/main/java/net/nosadnile/Main.java
@@ -0,0 +1,15 @@
+package net.nosadnile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
