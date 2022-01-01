fb72413cb7821ff8f237a1f76d5980ea93b4239d
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-01-01 21:27:29 +0000

    Support inserting & querying databases

--- flow-waterfall/src/main/java/net/nosadnile/flow/app/AppMain.java
+++ flow-waterfall/src/main/java/net/nosadnile/flow/app/AppMain.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
@@ -16,19 +17,26 @@ public class AppMain {
         DatabaseProvider db_ = new DatabaseProvider("localhost", "27017");
         MongoClient db = db_.getDB();
         DB flowDb = db.getDB("_nsn_flow-pre-alpha-testing");
-        List<DBObject> rw08ranks = new ArrayList<>();
-        rw08ranks.add(new BasicDBObject("_id", 0)
-        .append("name", "admin")
-        .append("prefix", "[Admin]")
-        .append("color", "Red")
-        .append("permissions", "net.nosadnile.flow.admin.*"));
-        DBObject player = new BasicDBObject("_id", "111dff66-087b-4948-bbd4-f47ed46af6ac")
-                                    .append("username", "RedstoneWizard08")
-                                    .append("level", 1)
-                                    .append("xp", 0)
-                                    .append("ranks", rw08ranks);
         DBCollection players = flowDb.getCollection("players");
-        players.insert(player);
+        //   ----------------------------- INSERTING -----------------------------
+        // List<DBObject> rw08ranks = new ArrayList<>();
+        // rw08ranks.add(new BasicDBObject("_id", 0)
+        // .append("name", "admin")
+        // .append("prefix", "[Admin]")
+        // .append("color", "Red")
+        // .append("permissions", "net.nosadnile.flow.admin.*"));
+        // DBObject player = new BasicDBObject("_id", "111dff66-087b-4948-bbd4-f47ed46af6ac")
+        // .append("username", "RedstoneWizard08")
+        // .append("level", 1)
+        // .append("xp", 0)
+        // .append("ranks", rw08ranks);
+        // players.insert(player);
+        //   ----------------------------- QUERYING -----------------------------
+        // DBObject query = new BasicDBObject("_id", "111dff66-087b-4948-bbd4-f47ed46af6ac");
+        // DBCursor cursor = players.find(query);
+        // DBObject rw08 = cursor.one();
+        // System.out.println(rw08.toString());
         db_.disable();
+        System.out.println("Execution finished.");
     }
 }
