c7d96e12723d63a1668d4c3ad3cf3abfbbe4a7e3
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-05-08 18:21:55 +0000

    nevermind xD

--- .space.kts
+++
@@ -1,8 +0,0 @@
-job("Build and publish") {
-    container(displayName = "Gradle publish", image = "gradle") {
-        kotlinScript { api ->
-            api.gradle("build")
-            api.gradle("getLoginFromEnv", "publish")
-        }
-    }
-}
