d3987fdeb3ecec9b4c1008e10291fe9c93527eee
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-05-08 18:07:25 +0000

    Fix gradle run

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("Build and publish") {
     container(displayName = "Gradle publish", image = "amazoncorretto:17-alpine") {
         kotlinScript { api ->
             api.gradlew("build")
-            api.gradlew("getLoginFromEnv publish")
+            api.gradlew("getLoginFromEnv", "publish")
         }
     }
 }
