839b6b8eb1355509579c3619e0ae9b4145ed6d7a
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-05-08 17:54:08 +0000

    Fix auth

--- .space.kts
+++ .space.kts
@@ -2,7 +2,8 @@ job("Build and publish") {
     container(displayName = "Gradle publish", image = "amazoncorretto:17-alpine") {
         kotlinScript { api ->
             api.gradlew("build")
-            api.gradlew("publish")
+            api.gradlew("flow-purpur:getLoginFromEnv")
+            api.gradlew("flow-purpur:publish")
         }
     }
 }
